bff4ce6c3e03a453ba058faa7d44d29526cb40b3
mdayubali88 <mohammadayubali88@gmail.com>
2023-07-30 11:25:08 +0600

    Code Updated

---
+++ tuple.py
@@ -0,0 +1,11 @@
+# tuple is same to the list expcept the () sign. 
+# list are muteable where tuples ase immutable. 
+
+myList = [1,3,4,54,5, 'Md Ali Ershad']
+my_tuples = (22,45,66,6,'md Ayub ali')
+
+myList[2]=  'New value added'
+
+# myTuples[3] = 'try to add' //will create error
+
+print(my_tuples[4])
